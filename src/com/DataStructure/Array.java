3d386fb9c45e78dc938820b94f27fb6d132e158f
Naveed Farouq <naveedfarouq@Naveeds-MacBook-Pro.local>
2025-02-01 18:13:02 +0000

    create insert method to add item to array and print method to print array

--- src/com/DataStructure/Array.java
+++ src/com/DataStructure/Array.java
@@ -5,9 +5,20 @@ import java.util.Arrays;
 public class Array {
 
     private int[] items;
+    private int count;
 
     public Array(int length) {
         items = new int[length];
     }
 
+    public void print() {
+        for (int i = 0; i < count; i++) {
+            System.out.print(Arrays.toString(items));
+        }
+    }
+
+    public void insert(int item) {
+        items[count] = item;
+        count++;
+    }
 }
--- src/com/DataStructure/Main.java
+++ src/com/DataStructure/Main.java
@@ -3,6 +3,8 @@ package com.DataStructure;
 public class Main {
     public static void main(String[] args) {
 
-
+        Array number = new Array(3);
+        number.insert(10);
+        number.print();
     }
 }
