e117bc458e187e9e90e166d52a2c68be644ce8af
Naveed Farouq <naveedfarouq@Naveeds-MacBook-Pro.local>
2025-02-01 17:56:05 +0000

    create array class

---
+++ src/com/DataStructure/Array.java
@@ -0,0 +1,13 @@
+package com.DataStructure;
+
+import java.util.Arrays;
+
+public class Array {
+
+    private int[] items;
+
+    public Array(int length) {
+        items = new int[length];
+    }
+
+}
